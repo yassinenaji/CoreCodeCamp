b6d074bf3dc0e6be01fa9d5e45d2eef5286e018a
Yassine Naji <shinobinaji@gmail.com>
2022-03-28 19:01:38 +0000

    add camp controller

---
+++ CoreCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+
+        public object Get()
+        {
+            return new { Slang = "Mamamia", country = "Italian" };
+        }
+
+    }
+}
